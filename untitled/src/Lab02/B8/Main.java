655379627270c59ea0ddf3ffdc176dcd5a899043
Minh Ha <minhha1192003@gmail.com>
2025-05-14 05:50:59 +0700

    Bai 8 ngay thang nam

--- untitled/src/Lab02/B8/Main.java
+++ untitled/src/Lab02/B8/Main.java
@@ -7,31 +7,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap nam kiem tra nam nhuan:");
         int year =sc.nextInt();
-        if((year %4 ==0 && year %100 !=0)|| (year %400 ==0))
+        boolean isLeap= ((year % 4==0 && year %100 !=0) || year %400 ==0 );
+        if (isLeap)
         {
             System.out.println("Nam nhuan");
         }
         else {
-            System.out.println("Nhap thang de kiem tra:");
-            int month= sc.nextInt();
-            if (month >=1 && month<=12)
-            {
-                switch (month)
-                {
-                    case 1:
-                        System.out.println("30 ngay");
-                        break;
-                    case 2:
+            System.out.println("Nam ko nhuan");
+        }
+        System.out.println("Nhap vao thang de kiem tra: ");
+        int month = sc.nextInt();
+        if (month >=1 && month<=12) {
+            switch (month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    System.out.println("31 ngay");
+                    break;
+                case 4: case 6: case 9: case 11:
+                    System.out.println("30 ngay");
+                    break;
+                case 2:
+                    if (isLeap)
+                    {
+                        System.out.println("29 ngay");
+                    }
+                    else {
                         System.out.println("28 ngay");
-                        break;
-                    case 3:
-                        System.out.println(" 30 ngay");
-                        break;
-                }
+                    }
+                    break;
             }
-            else {
-                System.out.println("nhap lai thang sao cho hop le");
-            }
+        }
+        else {
+            System.out.println("Nhap lai thang ");
         }
     }
 }
