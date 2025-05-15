38c35d443ef542375cee0ee0fa6316f7e3e5c5ca
Minh Ha <minhha1192003@gmail.com>
2025-05-15 16:01:26 +0700

    Bai10 lab03

---
+++ untitled/src/Lab03/B10/Main.java
@@ -0,0 +1,30 @@
+package Lab03.B10;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap 1 so n kiem tra :");
+        int n = scanner.nextInt();
+        int sum=0;
+        if (n>0) {
+            for (int i =1; i <n;i++)
+            {
+                if(n%i==0)
+                {
+                    sum+=i;
+                }
+            }
+
+            if (sum==n)
+            {
+                System.out.printf("%d la so hoan hao",n);
+            }
+            else {
+                System.out.println("Ko phai la so hoan hao");
+            }
+        }
+        else System.out.println("Nhap lai n");
+    }
+}
