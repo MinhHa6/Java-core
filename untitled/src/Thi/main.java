87c42ad26a382a5b94862b2e3617dae8d70d1012
Minh Ha <minhha1192003@gmail.com>
2025-05-27 21:42:30 +0700

    khuyen mai

--- untitled/src/Thi/main.java
+++
@@ -1,8 +0,0 @@
-package Thi;
-
-public class main {
-    public static void main(String[] args) {
-        tinh tinh1= new tinh();
-        tinh1.add(3,4);
-    }
-}
--- untitled/src/Thi/tinh.java
+++
@@ -1,9 +0,0 @@
-package Thi;
-
-public class tinh {
-    protected void add(int num1, int num2)
-    {
-        int sum=num1+num2;
-        System.out.printf("%d + %d=%d",num1,num2,sum);
-    }
-}
