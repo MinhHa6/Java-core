1664892b0bc465ff7c148ade87779e15551a16cb
Minh Ha <minhha1192003@gmail.com>
2025-06-03 23:08:13 +0700

    gendric and collection

---
+++ untitled/src/Java/Leson6.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leson6 {
+    public static void main(String[] args) {
+        /*
+        I.Mang
+
+         */
+        List temp =new ArrayList<>();
+        temp.add(1);
+        temp.add(2);
+        System.out.println(temp);
+    }
+}
