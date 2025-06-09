c63cddf23197e906597ce77309e1180cf06265d7
Minh Ha <minhha1192003@gmail.com>
2025-06-09 22:45:08 +0700

    Bai2 Flower

--- untitled/src/Lab08/B2/FlowerTest.java
+++ untitled/src/Lab08/B2/FlowerTest.java
@@ -3,6 +3,8 @@ package Lab08.B2;
 import Lab08.City;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class FlowerTest {
@@ -42,6 +44,44 @@ public class FlowerTest {
             flower.display();
         }
     }
+    public  void sapXepTheoTenHoa() {
+        Collections.sort(dshoa, new Comparator<Flower>() {
+            public int compare(Flower f1, Flower f2) {
+                return f1.getFlowerName().compareToIgnoreCase(f2.getFlowerName());
+            }
+        });
+        System.out.println("Đã sắp xếp xong theo tên hoa!");
+    }
+    public void hienThiHoaMauTrang() {
+        boolean found = false;
+        for (Flower f : dshoa) {
+            if (f.getColor().equalsIgnoreCase("trắng")) {
+                f.display();
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Không có loài hoa nào có màu trắng.");
+        }
+    }
+
+
+    public void timKiemFlowerId() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap flower Id can tim : ");
+        String id = scanner.nextLine();
+        boolean found = false;
+        for (Flower flower : dshoa) {
+            if (flower.getFlowerId().equalsIgnoreCase(id)) {
+                flower.display();
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Khong tim thay flower Id.");
+        }
+    }
+
 
     public static void main(String[] args) {
         FlowerTest flowerTest= new FlowerTest();
@@ -51,8 +91,8 @@ public class FlowerTest {
             System.out.println("1.Nhap thong tin n loai hoa");
             System.out.println("2.Hien thi");
             System.out.println("3.Sap xep theo danh sach tang dan ");
-            System.out.println("4.Tim kiem theo flower Id");
-            System.out.println("5.Hien thi tat ca cac loai hoa co mau trang ");
+            System.out.println("4.Hien thi tat ca cac loai hoa co mau trang ");
+            System.out.println("5.Tim kiem theo flower Id");
             System.out.println("6.Thoat");
             Scanner scanner= new Scanner(System.in);
             int choice = scanner.nextInt();
@@ -66,6 +106,15 @@ public class FlowerTest {
                     flowerTest.hienThi();
                     break;
                 case 3:
+                    flowerTest.sapXepTheoTenHoa();
+                    break;
+                case 4:
+                    flowerTest.hienThiHoaMauTrang();
+                    break;
+                case 5:
+                    flowerTest.timKiemFlowerId();
+                    break;
+                case 6:
                     break;
             }
         }
