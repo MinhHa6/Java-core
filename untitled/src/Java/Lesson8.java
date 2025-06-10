fc928dc935cc580349ee16a82d728e6932cdeb47
Minh Ha <minhha1192003@gmail.com>
2025-06-10 19:51:39 +0700

    interface,astract,nested

---
+++ untitled/src/Java/Lesson8.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Lesson8  implements Lesson8_IdemoInterface{
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void input() {
+
+    }
+}
---
+++ untitled/src/Java/Lesson8_IdemoInterface.java
@@ -0,0 +1,8 @@
+package Java;
+
+public interface Lesson8_IdemoInterface {
+    final float pi=3.14f;
+    static  String content ="hello";
+    void show();
+    void input();
+}
