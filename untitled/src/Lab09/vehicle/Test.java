674546fce9de32a6bde70f33096027f3cafb7346
Minh Ha <minhha1192003@gmail.com>
2025-06-11 23:56:29 +0700

    B1

---
+++ untitled/src/Lab09/vehicle/Test.java
@@ -0,0 +1,15 @@
+package Lab09.vehicle;
+
+public class Test {
+    public static void main(String[] args) {
+        while (true)
+        {
+            System.out.println("=====Menu=======");
+            System.out.println("1.Input");
+            System.out.println("2.Display");
+            System.out.println("3.Sort by price ");
+            System.out.println("4.Search by model");
+            System.out.println("5.Exit");
+        }
+    }
+}
---
+++ untitled/src/Lab09/vehicle/car/Car.java
@@ -0,0 +1,12 @@
+package Lab09.vehicle.car;
+
+import Lab09.vehicle.vehicles;
+
+public class Car extends vehicles {
+    String color;
+    public Car (String color)
+    {
+        super();
+        this.color=color;
+    }
+}
