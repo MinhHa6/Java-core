0b9fcda60a5dc50ff03b94988f69d5780495aac3
Minh Ha <minhha1192003@gmail.com>
2025-06-12 19:12:54 +0700

    lesson9

---
+++ untitled/src/Java/Lesson09/Main.java
@@ -0,0 +1,17 @@
+package Java.Lesson09;
+
+public class Main {
+    /*
+    //I.interface
+    field:static/filnal
+    method:phuong thuc truu tuong ko than luon luon la public
+    thuc thi:implement nhieu interface khac
+    dat ten:bat dau bang chu I
+    class thuc thi interface :nhung class phai overide toan bo interface
+    II.astract class
+    filed; su dung binh thuong
+    method:co the phuong thuc binhthuong va vua co phuong thuc truu tuong
+    de su dung duoc phuong phai co botu astract
+    III.Nested lop trong lop
+     */
+}
