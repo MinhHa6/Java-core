d9c449de2a20a495e0f0cd6ba2573d01ab77fad5
Minh Ha <minhha1192003@gmail.com>
2025-06-16 23:14:28 +0700

    Bai 3

---
+++ untitled/src/Lab10/country/Country.java
@@ -0,0 +1,8 @@
+package Lab10.country;
+
+public class Country {
+    private  String countryName;
+    private double area;
+    private long numbersPerson;
+    
+}
---
+++ untitled/src/Lab10/country/ICountry.java
@@ -0,0 +1,7 @@
+package Lab10.country;
+
+public interface ICountry {
+    public void input();
+    public  void display();
+    public float averageArea(double area ,long numbersPreson);// tinh dien tich tren 1 dau nguoi
+}
