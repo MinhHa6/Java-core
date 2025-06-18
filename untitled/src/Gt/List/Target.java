f6163f2c6c0511a1808efac9ef4ae5ce734d1631
Minh Ha <minhha1192003@gmail.com>
2025-06-18 23:27:34 +0700

    tong phan tu trong mang

--- untitled/src/Gt/List/Target.java
+++ untitled/src/Gt/List/Target.java
@@ -1,4 +1,30 @@
 package Gt.List;
 
 public class Target {
+    public static int []twoSum(int []nums,int target)
+    {
+        for (int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]==target-nums[i])
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int []{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 6, 7};
+        int[] result = twoSum(nums, 3);
+
+        if (result.length == 3) {
+            System.out.println("Vị trí: " + result[0] + " và " + result[1]);
+        } else {
+            System.out.println("Không tìm thấy cặp số nào.");
+        }
+
+    }
 }
