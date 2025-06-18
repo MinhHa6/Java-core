66a2f67fc119b5841491abee178e1d9c56517b51
Minh Ha <minhha1192003@gmail.com>
2025-06-19 01:05:33 +0700

    map

--- untitled/src/Lab07/collection/MapDemo.java
+++ untitled/src/Lab07/collection/MapDemo.java
@@ -1,4 +1,24 @@
 package Lab07.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapDemo {
+    public static void main(String[] args) {
+        // tao doi tuong map va chen du lieu
+        Map<String,String>map= new HashMap<>();
+        map.put("kv1","MH3");
+        map.put("kv2","MH23");
+        map.put("kv3","MH2");
+        map.put("kv4","MH1");
+        for (Map.Entry<String,String> entrySet:map.entrySet())
+                {
+                    String key=entrySet.getKey();
+                    String value=entrySet.getValue();
+                    System.out.println(key+"-"+value);
+                }
+        System.out.println("Key"+map.get("kv1"));
+        System.out.println(map.remove("kv2"));
+        System.out.println("Sau khi xoa:"+map);
+    }
 }
