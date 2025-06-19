593386f781142afe9cd7d56b64cb3dd7264961a2
Minh Ha <minhha1192003@gmail.com>
2025-06-19 23:59:21 +0700

    set-Treeset phan tu ko trung ,ko lay null,sapxep

--- untitled/src/Lab07/Set/Treeset.java
+++ untitled/src/Lab07/Set/Treeset.java
@@ -1,4 +1,23 @@
 package Lab07.Set;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Treeset {
+    public static void main(String[] args) {
+        Set<Integer>setTree= new TreeSet<>();
+        //Treeset ko lay phan tu trung lap,tu dong sap xep cac phan tu
+        setTree.add(8);
+        setTree.add(4);
+        setTree.add(4);
+        setTree.add(5);
+        setTree.add(6);
+        setTree.add(7);
+        System.out.println(setTree);
+        // kiem tra phan tu co trong set ko
+        System.out.println(setTree.contains(4));
+        // xoa phan tu trong treeset
+        System.out.println(setTree.remove(4));
+        System.out.println(setTree);
+    }
 }
