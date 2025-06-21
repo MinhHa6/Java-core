817b074eea4f8b45c969855d4b464d77f272a63f
Minh Ha <minhha1192003@gmail.com>
2025-06-21 23:19:36 +0700

    jsjjs

--- untitled/src/lab04.java
+++ untitled/src/lab04.java
@@ -2,5 +2,6 @@ public class lab04 {
     public static void main(String[] args) {
         System.out.println("class+1");
         System.out.println("vu misnnhs ha");
+        System.out.println("hdsjsjjjjs");
     }
 }
