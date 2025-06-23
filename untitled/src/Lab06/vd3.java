fdcf22ab4b944c4855457d20fce7d7e64b45beed
Minh Ha <minhha1192003@gmail.com>
2025-06-23 21:47:09 +0700

    sap xep va hien thi theo ds amphabet

--- untitled/src/Lab06/vd3.java
+++ untitled/src/Lab06/vd3.java
@@ -1,6 +1,7 @@
 package Lab06;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class vd3 {
     ArrayList<String> stMonHoc = new ArrayList<>();
@@ -14,10 +15,20 @@ public class vd3 {
         for (int i=0;i<stMonHoc.size();i++)
         {
             String get=stMonHoc.get(i);
-            System.out.println((i+1)+get);
+            System.out.println((i+1)+"-"+get);
+        }
+        // sap xep theo ds amphabet
+        Collections.sort(stMonHoc);
+        System.out.println("Ds sau khi duoc sap xep la:");
+        for (int i=0;i<stMonHoc.size();i++)
+        {
+            String get=stMonHoc.get(i);
+            System.out.println((i+1)+"-"+get);
         }
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        vd3 vs = new vd3();
+        vs.test();
     }
 }
