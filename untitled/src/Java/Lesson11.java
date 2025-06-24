f88f8928ac61f8904e2d3eee94c8bb66f6a4e34b
Minh Ha <minhha1192003@gmail.com>
2025-06-24 19:26:45 +0700

    File

---
+++ untitled/src/Java/Lesson11.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.io.File;
+
+public class Lesson11 {
+/*
+Exception
+//1.khai niem
+-loi xay ra khi chuong trinh khoi chay co the do ban than hoac loi noi dung
+ hoac so loi ko mong muon khi co input hoac output
+=> xu dung exception khi doan code co the xay ra loi
+//2.systax
+//try { cac cau lenh xu ly co the phat sinh loi}
+catch (ten_ngoai_le bien){khi mot trong so cac lenh bi loi yhi cac khoi lenh trong try catch thuc thi }
+..finnaly  { se thuc thi dam bao cac cau lenh tiet kiem tai nguyen  }
+-luu y :loi lon nhat dc viet sau cac loi lon hon
+-loi exception la loi lon nhat
+throw
+-Dingh nghia :lai exception
+ */
+public static void main(String[] args) {
+    File file = new File("F:minhhha.txt");
+}
+}
