d0b15d1dac749dd7132a2b603fc26eca0d9701aa
Minh Ha <minhha1192003@gmail.com>
2025-09-11 21:47:31 +0700

    oop -list

---
+++ untitled/src/OT/Collection/B5/Product.java
@@ -0,0 +1,8 @@
+package OT.Collection.B5;
+
+public class Product {
+    private  String id;
+    private String name;
+    private Double price;
+
+}
---
+++ untitled/src/OT/Collection/B5/Store.java
@@ -0,0 +1,10 @@
+package OT.Collection.B5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<Product>list= new ArrayList<>();
+    
+
+}
