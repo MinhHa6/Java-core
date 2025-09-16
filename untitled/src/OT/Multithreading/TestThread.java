89ccc41f53086ef4c69bd69277e2f6f0c4213536
Minh Ha <minhha1192003@gmail.com>
2025-09-16 22:10:05 +0700

    Thread

---
+++ untitled/src/OT/Multithreading/TestThread.java
@@ -0,0 +1,20 @@
+package OT.Multithreading;
+
+public class TestThread {
+    public static void main(String[] args) {
+        Demo pd= new Demo();
+        ThreadDemo T1= new ThreadDemo("Thread-1",pd);
+        ThreadDemo T2 = new ThreadDemo("Thread-2",pd);
+        T1.start();
+        T2.start();
+        // wait for threads to end;
+        try{
+            T1.join();
+            T2.join();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Interrupted");
+        }
+    }
+}
---
+++ untitled/src/OT/Multithreading/ThreadDemo.java
@@ -0,0 +1,29 @@
+package OT.Multithreading;
+
+public class ThreadDemo extends Thread{
+    private Thread t;
+    private String threadName;
+    Demo pd;
+    ThreadDemo (String name,Demo d)
+    {
+        threadName=name;
+        pd=d;
+    }
+    public void run()
+    {
+        synchronized (pd){
+            pd.printCount();
+        }
+        System.out.println("thread"+threadName+"exiting");
+    }
+    public void start()
+    {
+        System.out.println("Starting"+threadName);
+        if(t==null)
+        {
+            t= new Thread(this,threadName);
+            t.start();
+        }
+    }
+
+}
