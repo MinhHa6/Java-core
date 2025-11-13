9b981e02a22a7288698bfed6eff10464d8c39a68
Minh Ha <minhha1192003@gmail.com>
2025-11-13 23:45:39 +0700

    collection

---
+++ untitled/src/core/javacollection/JavaCollection.java
@@ -0,0 +1,12 @@
+package core.javacollection;
+/*
+1.Java Collection
+-javacollection la 1 farmwork trong java dung de quan ly va thao tac voi nhom doi tuong.No cung cap 1 tap hop cac cau truc du lieu va cac thuat toan
+de xu ly du lieu nhu them,xoa,duyet qua va sap xep cac phan tu .Java collection bao gom interface va classes giup lap trinh vien de qua ly du lieu
+theo cach hieu qua hon.
+2.interface
+-collection : interface co so cho tat ca cac tap hop. No dinh nghia cac phuong thuc co ban nhu add(),remove(),size(),iteratoe(),...
+-List:Interface mo rong tu collection, dai dien 
+ */
+public class JavaCollection {
+}
--- untitled/src/core/javastring/JavaString.java
+++ untitled/src/core/javastring/JavaString.java
@@ -8,6 +8,17 @@ trong java String co ban la mot doi tuong bieu dien chuoi cac gia tri char.Mot m
 tao mot string trong Java bang cach su dung 3 lop nay/
 -Java String la bat bien nghia la ko the thau doi kich thuoc cu ano.Bat cu khi nao chung ta thay doi bat ky String nao thi mot istance moi se dc
 tao ra .Neu ban muon su dung cac chuoi co the thay doi thi co the dung stringbuider va stringbuffer
+2.De khoi tao 1 java String
+a.su dung String literal
+String dc tao ra voi dau ""
+String s1="well come vu minh ha"
+-moi lan tao ra 1 string jvm se kiem tra " String constant pool" truoc neu string da ton tai trong pool thi 1 tham chieu den instance do se dc tao ra .neu String do
+chua ton tai thi jvm se tao String moi
+* tai sao dung String literal
+de lam java su dung bo nho hieu qua hon,boi vi khong co doi tuong  moi nao dc tao neu no da ton tai trong Spring constant pool
+b. su dung tu khoa new
+Trong trường hợp như vậy, JVM sẽ tạo một đối tượng String mới trong bộ nhớ heap bình thường (không phải pool) và giá trị “Welcome to Tây Java” theo nghĩa đen sẽ được đặt trong Spring constant pool.
+ Biến s sẽ tham chiếu đến đối tượng trong heap (không phải pool).
 
  */
 public class JavaString {
@@ -15,5 +26,11 @@ public class JavaString {
         char []ch ={'v','u','m','i','n','h','h','a'};
         String s= new String(ch);
         System.out.println(s);
+        String s1= "welcome";
+        String s2= "welcome";// jvm ko tao 1 String moi
+        System.out.println(s2+s1);
+
+        // tao doi tuogn String moi bang  new
+        String s3 = new String("welcome");
     }
 }
