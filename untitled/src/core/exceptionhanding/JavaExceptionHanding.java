9f742208132421066e8a0d3836f8f28917bffa88
Minh Ha <minhha1192003@gmail.com>
2025-11-16 23:44:31 +0700

    .

---
+++ untitled/src/core/exceptionhanding/JavaExceptionHanding.java
@@ -0,0 +1,23 @@
+package core.exceptionhanding;
+/*
+Exceptionhanding trong java la co che xu ly loi xay ra trong ung dung de dam bao ung dung hoat dong on dinh va tranh bi gian doan dot ngot.Java cung cap cac tu
+khoa de xu ly ngoai le bao gom :try,catch,finally,throw, va throws
+1. cau truc cua lop JavaException
+Lop java.lang.throwable la lop cua he thong phan cap javaException dc ke thua boi 2 lop con:Exception va error
+2.cac laoi exception trong java
+trong java,exception dc chia thanh 2 loai chinh
+-checked Exception:day la nhung ngoai le ma trinh bien dich bat buoc phai xu ly (thuong la ngoai le co the xay ra trong qua trinh doc file,
+ket noi co so du lieu,...) vi du:Ioexception,SqlException
+-Unchecked Excepotion :La nhung ngoai le xay ra khi loi lap trinh va ko bi trinh vien dich kiem tra.Thuong la cac loi logic trong chuong trinh,vidu:
+ArithmeticException, NullPointerException, ArrayIndexOutOfBoundsException.
+3. Cac tu khoa chinh
+try :dc su dung de bao boc doan ma phat sinh ngoai le
+catch :dung de xu xy cac ngoai le phat sinh trong khoi try.Khi ngoai le xay ra,Java se tim khoi catch tuong ung xu ly.
+fillnaly: khoi lenh nay luon dc thuc thi du ngoai le co xay ra hay ko.No thuogn dc dung de don dep tai nguyen nhu dong tap hoac ket noi co so du lieu
+throw:duoc dung de nem ra 1 ngoai le mot cach ro rang trong chuong trinh
+throws:khai bao ngoai le co the phat sinh trong phuong thuc de phuong thuc goi bt rang ngoai le co the xay ra va phai dc xu ly .
+
+
+ */
+public class JavaExceptionHanding {
+}
