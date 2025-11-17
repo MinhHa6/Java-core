b5729f821ebc7210accb7cef46080233521f3671
Minh Ha <minhha1192003@gmail.com>
2025-11-18 02:20:02 +0700

    networking

--- untitled/src/core/networking/Networking.java
+++ untitled/src/core/networking/Networking.java
@@ -1,4 +1,28 @@
 package core.networking;
-
+/*
+- javanetworking la 1 phan quan trong trong lap trinh Java cho phep cac ung dung giao tiep vs nhau thong qua mang may tinh.java cung cap san cac packages va
+api de ho tro xay dung ung dung mang nhu client-server,peer-to-peer hay phan tan
+1.Goi java.net
+goi java.net chua class va interface va ho tro lap trinh mang.mot so lop tieu bieu
+-InetAddress: Dai dien cho 1 dia chi Ip
+UrL: dai dien cho uniform resource locator, cho phep truy xuat du lieu tu tai nguyen qua mang
+UrlConnection :Dai dien cho ket noi den mot tai nguyen dc chi dinh boi Url
+Socet:Dung de ket noi TCP giau client va server
+Server Socket :Dung de tao server va lang nghe cac ket noi tu TCP tu client
+datagramSocket va DatagramPacket:Dung cho ket noi UDp, ho tro gui nhan cac packet ko yeu cau ket noi
+2. Cac giao thuc mang on dinh
+TCP;Giao thuc huong ket noi, dam bao du lieu truyen tai an toan, day du theo dung thu tu
+UDP: Giao thuc ko ket noi, nhanh hon TCp, nhung ko dam bao du lieu toan venn (Co the mat goi hoac sai thu tu)
+3. Lap trinh Soc ket trong Java
+Java ho tro lap trinh socket cho ca TCP va Udp, giup xay dung cac ung dung client-server
+TcP sockets
+tcp la giao thuc
+4. Giao tiep qua Url
+Java ho tro giao tiep voi tai nguyen URl bang cac lop Url va Url connection
+5. Loi ich va ung dung cua Java netWorking
+-Tinh doc lap nen tnag:Java chay tren nhieu he dieu hanh, giup ung dung mang de dang trien khai da nen tang
+- ho tro giao thuc chuan:Ho tro manh me cac giao thuc nhu HTTP<FTP<TCP,UDP
+- tinh bao mat cao :Java cung cap api bao mat manh me (SSL/TLS, ma hoa, xac thuc)
+ */
 public class Networking {
 }
