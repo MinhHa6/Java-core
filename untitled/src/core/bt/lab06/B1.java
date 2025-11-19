bd78e2e2c9c70bedae2bea51eb826490d66464d2
Minh Ha <minhha1192003@gmail.com>
2025-11-19 23:49:19 +0700

    bt

---
+++ untitled/src/core/bt/lab06/B1.java
@@ -0,0 +1,22 @@
+package core.bt.lab06;
+
+import java.util.Random;
+// nhap mang co n phan tu nhap thu cong 
+// nhap 1 mang co 10 phan tu cac so nguyen, hien thi mang vu anhap ra man hinh
+// nhap tu dong dung random mang co 10 phan tu
+public class B1 {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5,6,7,8,9,10};
+        for (int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        Random r = new Random();
+        int arrs[]=new int[3];
+        for (int i=0;i<arrs.length;i++)
+        {
+            arrs[i]=r.nextInt(100);
+            System.out.println(arrs[i]);
+        }
+    }
+}
