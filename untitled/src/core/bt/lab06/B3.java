b4d51e48f0adea13b071a8e14caafc0fcbf3778f
Minh Ha <minhha1192003@gmail.com>
2025-11-22 19:33:01 +0700

    :

--- untitled/src/core/bt/lab06/B3.java
+++ untitled/src/core/bt/lab06/B3.java
@@ -3,6 +3,10 @@ package core.bt.lab06;
 import java.util.Scanner;
 
 // nhap mang co n phan tu cac nguyen to , In ra cac phan tu la so nguyen cua mang
+// nhap mang n phan tu in ra cac phan tu la so chan
+// sap xep mang bang thuat toan chon truc tiep (selection sort)
+// sap xep mang tang dan bang thuat toan noi bot
+// sap xep mang tang dan bang thuat toan chen
 public class B3 {
     static  boolean soNguyenTo(int n)
     {
@@ -16,7 +20,90 @@ public class B3 {
         }
         return true;
     }
+   static void  soChan(int arr [])
+    {
+        for (int i=0;i<arr.length;i++)
+        {
+            if(arr[i] % 2 ==1)
+            {
+                System.out.println(arr[i]+" ");
+            }
+        }
+        System.out.println();
+    }
+    static void  selectionSort(int arr [])
+    {
+        int n= arr.length;
+        for (int i=0; i<n-1;i++)
+        {
+            int minIndex= i;
+           for (int j=i+1;j<n;j++)
+           {
+               if(arr[j]<arr[minIndex])
+               {
+                   minIndex =j;
+               }
+           }
+            // Hoán đổi
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
 
+            // In mảng sau mỗi vòng i
+            System.out.print("Vong " + (i+1) + ": ");
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+     }
+static void blueSort(int arr [])
+{
+    int n= arr.length;
+    for(int i=0;i<n-1;i++)
+    {
+        for (int j=0;j<n-i-1;j++)
+        {
+            if (arr[j] < arr[j+1])
+            {
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        // in mang sau moi vong lap i
+        System.out.println("Sau vong"+(i+1)+":");
+        for (int k=0;k<n;k++)
+        {
+            System.out.println(arr[k]+"");
+        }
+        System.out.println();
+    }
+}
+    static void insertionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // phần tử cần chèn
+            int j = i - 1;
+
+            // Dời các phần tử lớn hơn key sang phải
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // Chèn key vào vị trí đúng
+            arr[j + 1] = key;
+
+            // In mảng sau mỗi bước
+            System.out.print("Sau vong " + i + ": ");
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Nhap n phan tu cua mang:");
@@ -34,5 +121,13 @@ public class B3 {
                 System.out.println(arr[i]+" ");
             }
         }
+        System.out.println("So le  cua mang la:");
+        soChan(arr);
+        System.out.println("Mang tang  dan cua mang la:");
+        selectionSort(arr);
+        System.out.println("Mang sap xep giam dan la :");
+        blueSort(arr);
+        System.out.println("Mang sap xep la:");
+        insertionSort(arr);
     }
 }
